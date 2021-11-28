c78fff82fafc11f371ee7b47c1c404b28d8ad4a9
Erik Luebbers <erluebbers@gmail.com>
2021-11-28 10:09:06 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Your code here
+
+function createEmployeeRecord ([firstName, familyName, title, payPerHour]) {
+  let employeeRecord = {
+    firstName: firstName,
+    familyName: familyName,
+    title: title,
+    payPerHour: payPerHour,
+    timeInEvents: [],
+    timeOutEvents: []
+  }
+  return employeeRecord
+}
+
+function createEmployeeRecords (array1, array2, ...arrays) {
+  let employeeRecords = [
+    createEmployeeRecord(array1),
+    createEmployeeRecord(array2),
+    createEmployeeRecord(arrays)
+  ]
+  return employeeRecords
+}
