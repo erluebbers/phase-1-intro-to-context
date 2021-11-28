85115e426695185621fe951706b944a96bd5a62e
Erik Luebbers <erluebbers@gmail.com>
2021-11-28 13:35:14 -0800

    Done.

--- index.js
+++ index.js
@@ -11,11 +11,60 @@ function createEmployeeRecord ([firstName, familyName, title, payPerHour]) {
   return employeeRecord
 }
 
-function createEmployeeRecords (array1, array2, ...arrays) {
-  let employeeRecords = [
-    createEmployeeRecord(array1),
-    createEmployeeRecord(array2),
-    createEmployeeRecord(arrays)
-  ]
+
+function createEmployeeRecords (array) {  
+  let employeeRecords = []
+  array.forEach(employee => {
+    employeeRecords.push(createEmployeeRecord(employee))
+  })
   return employeeRecords
+}
+
+function createTimeInEvent (employeeRecord, dateString) {
+  let timeIn = {
+    type: "TimeIn",
+    hour: parseInt(dateString.slice(11)),
+    date: dateString.slice(0, 10)
+  }
+  employeeRecord.timeInEvents.push(timeIn)
+  return employeeRecord
+}
+
+function createTimeOutEvent (employeeRecord, dateString) {
+  let timeOut = {
+    type: "TimeOut",
+    hour: parseInt(dateString.slice(11)),
+    date: dateString.slice(0, 10)
+  }
+  employeeRecord.timeOutEvents.push(timeOut)
+  return employeeRecord
+}
+
+function hoursWorkedOnDate (employeeRecord, date) {
+  let start
+  let end
+  for (const obj of employeeRecord.timeInEvents) {
+    if (obj.date === date) {
+      start = obj.hour
+    }
+  }
+  for (const obj of employeeRecord.timeOutEvents) {
+    if (obj.date === date) {
+      end = obj.hour
+    }
+  }
+  return (end - start)/100
+}
+
+function wagesEarnedOnDate (employeeRecord, date) {
+  return hoursWorkedOnDate(employeeRecord, date) * employeeRecord.payPerHour
+}
+
+function allWagesFor (employeeRecord) {
+  let wagesArray = []
+  let dateArray = employeeRecord.timeInEvents.filter(array => array.date)
+  for (const value of dateArray) {
+    wagesArray.push(hoursWorkedOnDate(employeeRecord, value))
+  }
+  return wagesArray.reduce((a, b) => a + b, 0)
 }
